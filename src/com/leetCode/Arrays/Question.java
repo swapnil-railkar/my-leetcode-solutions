16027340a7cb4582a29b6da24e40fe258cdc6ac2
Swapnil Railkar <swapnilrailkar01@gmail.com>
2024-10-27 10:35:19 +0530

    P17 accepted (rt : 1ms)

--- src/com/leetCode/Arrays/Question.java
+++ src/com/leetCode/Arrays/Question.java
@@ -1,8 +1,17 @@
 package com.leetCode.Arrays;
 
+import java.util.List;
+
+import com.leetCode.String.P17;
+
 public class Question {
     public static void main(String[] args) {
-        
+        P17 problem = new P17();
+        String num = "2356";
+        List<String> combinations = problem.solution(num);
+        for(String combination: combinations) {
+        	System.out.println(combination);
+        }
     }
 
 }
--- src/com/leetCode/String/P17.java
+++ src/com/leetCode/String/P17.java
@@ -2,16 +2,61 @@ package com.leetCode.String;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.TreeSet;
 
 public class P17 {
 
 	public List<String> solution(String numbers) {
-		return Collections.emptyList();
+		List<String> combinations = new ArrayList<>();
+		combinations = Collections.emptyList();
+		
+		for(int i=0; i< numbers.length(); i++) {
+			char num = numbers.charAt(i);
+			List<String> letters = cache(num);
+			combinations = merge(combinations, letters);
+		}
+		return combinations;
+	}
+	
+	private List<String> merge(List<String> combinations, List<String> letters) {
+		if (combinations.isEmpty()) {
+			return letters;
+		}
+		
+		List<String> newCombinations = new ArrayList<>();
+	
+		for(String combination: combinations) {
+			for(String letter: letters) {
+				String newCombination = combination + letter;
+				newCombinations.add(newCombination);
+			}
+		}
+		
+		return newCombinations;
+	}
+	
+	private List<String> cache(char key) {
+		
+		switch(key) {
+		case '2':
+			return List.of("a","b","c");
+		case '3':
+			return List.of("d","e","f");
+		case '4':
+			return List.of("g","h","i");
+		case '5':
+			return List.of("j","k","l");
+		case '6':
+			return List.of("m","n","o");
+		case '7':
+			return List.of("p","q","r","s");
+		case '8':
+			return List.of("t","u","v");
+		case '9':
+			return List.of("w","x","y","z");
+		default:
+			return Collections.emptyList();
+		}
+		
 	}
 }
