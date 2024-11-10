988962c42a1b5cde3260cebd5386b06162946f75
Swapnil Railkar <swapnilrailkar01@gmail.com>
2024-11-10 12:43:21 +0530

    P10 not accepted apporach 2

--- src/com/leetCode/Arrays/Question.java
+++ src/com/leetCode/Arrays/Question.java
@@ -5,8 +5,8 @@ import com.leetCode.String.P10;
 public class Question {
     public static void main(String[] args) {
         P10 question = new P10();
-        String str = "aab"; // false
-        String pattern = "c*a*b";
+        String str = "aaa"; // true
+        String pattern = "a*a";
         
         boolean answer = question.solution(str, pattern);
         System.out.println(answer);
--- src/com/leetCode/String/P10.java
+++ src/com/leetCode/String/P10.java
@@ -7,39 +7,42 @@ public class P10 {
 
 	public boolean solution(String str, String pattern){
 		try {
-			int stringPtr = 0;
-			for (int i=0; i< pattern.length(); i++) {
-				if (pattern.charAt(i) == str.charAt(stringPtr)) {
-					stringPtr++;
+			int strPtr = 0;
+			for(int i=0 ; i< pattern.length(); i++) {
+				if (pattern.charAt(i) == str.charAt(strPtr)) {
+					strPtr++;
 				} else {
-					if (pattern.charAt(i) == '.') {
-						stringPtr++;
-						continue;
+					if (pattern.charAt(i) == '.' ) {
+						strPtr++;
 					} else if (pattern.charAt(i) == '*') {
-
-						char prevChar = pattern.charAt(i - 1);
-						while(stringPtr < str.length() 
-								&& (str.charAt(stringPtr) == prevChar || prevChar == '.')) {
-							stringPtr++;
+						while(strPtr < str.length() && 
+								(pattern.charAt(i - 1) == str.charAt(strPtr) || pattern.charAt(i - 1) == '.') ) {
+							strPtr++;
 						}
-
 					} else {
-						//pattern.charAt(patternPtr) =/= str.charAt(i) 
-						if (pattern.charAt(i + 1) == '*') {
+						if(pattern.charAt(i + 1) == '*') {
 							i++;
-							continue;
 						} else {
 							return false;
 						}
-
 					}
 				}
 			}
-			return true;
-		}catch (Exception e) {
+			return strPtr == str.length() && lastCharacterMatches(str, pattern);
+		} catch(Exception e) {
 			return false;
 		}
-
+	}
+	
+	private boolean lastCharacterMatches(final String str, final String pattern) {
+		int strLastIndex = str.length() - 1;
+		int ptrLastIndex = pattern.length() - 1;
+		
+		if (pattern.charAt(ptrLastIndex) == '*' || pattern.charAt(ptrLastIndex) == '.') {
+			return true;
+		} else {
+			return str.charAt(strLastIndex) == pattern.charAt(ptrLastIndex);
+		}
 	}
 
 }
