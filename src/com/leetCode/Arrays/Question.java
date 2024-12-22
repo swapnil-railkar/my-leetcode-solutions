4920f1431923bffbb15e140a9f29dd18bb300b5a
Swapnil Railkar <swapnilrailkar01@gmail.com>
2024-12-22 14:40:42 +0530

    P316 not accepted (280/290 tests passing)

--- src/com/leetCode/Arrays/Question.java
+++ src/com/leetCode/Arrays/Question.java
@@ -5,8 +5,9 @@ import com.leetCode.P316;
 public class Question {
     public static void main(String[] args) {
         P316 question = new P316();
-        String answer = question.solution("cdadabcc"); //adbc
+        String answer = question.solution("rusrbofeggbbkyuyjsrzornpdguwzizqszpbicdquakqws"); //bfegkuyjor
         System.out.println(answer);
+        System.out.println("bfegkuyjorndiqszpcaw");
     }
 
 }
--- src/com/leetCode/P316.java
+++ src/com/leetCode/P316.java
@@ -1,29 +1,56 @@
 package com.leetCode;
 
 import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 import java.util.ArrayList;
 
 public class P316 {
 
 	public String solution(String str) {
-		List<Character> charList = new ArrayList<>();
+		Set<Character> uniqueChars = new TreeSet<>();
 		for(int i=0; i< str.length(); i++) {
-			addCharToList(charList, str.charAt(i));
+			uniqueChars.add(str.charAt(i));
 		}
-		return convertToString(charList);
+		List<Character> answer = getSetStartingWithMinChar(uniqueChars, str);
+		return convertToString(answer);
 	}
 	
-	public void addCharToList(List<Character> charList, char currChar) {
-		int index = charList.indexOf(currChar);
-		if(index != -1) {
-			if(index + 1 < charList.size() && charList.get(index + 1) < currChar) {
-				charList.remove(index);
-				charList.add(currChar);
-			}
-		} else {
-			charList.add(currChar);
-		}
+private List<Character> getSetStartingWithMinChar(Set<Character> uniqueChars, String str) {
 		
+		for(char ch : uniqueChars) {
+			List<Character> list = new ArrayList<>();
+			list.add(ch);
+			int firstIndex = str.indexOf(ch);
+			for(int i = firstIndex + 1; i< str.length(); i++) {
+				int index = list.indexOf(str.charAt(i));
+				if(index != -1) {
+					if(str.charAt(i) == ch) {
+						continue;
+					}
+					for(int j = index + 1; j< list.size(); j++) {
+                        if(list.get(j) > str.charAt(i)) {
+                        	// if we find out that list.get(j) will be removed in future 
+                        	// and after index j in list there exists character which is less than str.charAt(i)
+                        	// then we should remove str.charAt(i) from current index and it at end of current list state
+                        	
+                            break;
+                        } else if (list.get(j) < str.charAt(i)) {
+							list.remove(index);
+							list.add(str.charAt(i));
+							break;
+						}
+					}
+				} else {
+					list.add(str.charAt(i));
+				}
+			
+			}
+			if(list.size()== uniqueChars.size()) {
+				return list;
+			}
+		}
+		return new ArrayList<>(uniqueChars);
 	}
 	
 	private String convertToString(List<Character> charList) {
