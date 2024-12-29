692defd82dab813d6cba31b27090ee0fed00ad7b
Swapnil Railkar <swapnilrailkar01@gmail.com>
2024-12-29 22:46:27 +0530

    P162 accepted (rt: 0ms)

--- src/com/leetCode/Question.java
+++ src/com/leetCode/Question.java
@@ -2,11 +2,11 @@ package com.leetCode;
 
 public class Question {
 	public static void main(String[] args) {
-		P153 question = new P153();
-		int[] arr = {2,3, 1};
+		P162 question = new P162();
+		int[] arr = {1,2,3,1};
 
-		int minNum = question.solution(arr);
-		System.out.println(minNum);
+		int index = question.solution(arr);
+		System.out.println(index);
 	}
 
 }
