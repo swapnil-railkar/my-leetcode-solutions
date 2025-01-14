caba7c914f16038fa0df9a363289bc65e7ae9a3d
Swapnil Railkar <swapnilrailkar01@gmail.com>
2025-01-14 21:20:58 +0530

    P2657 accepted (rt: 1ms)

--- src/com/leetCode/P2657.java
+++ src/com/leetCode/P2657.java
@@ -1,20 +1,21 @@
 package com.leetCode;
 
-import java.util.Set;
-import java.util.HashSet;
-
 public class P2657 {
 	
 	public int[] solution(int[] A, int[] B) {
-        Set<Integer> a_set = new HashSet<>();
-        Set<Integer> b_set = new HashSet<>();
+		int[] cache = new int[A.length];
         int[] count = new int[A.length];
+        int sameNumsCount = 0;
         for(int i = 0; i< A.length; i++) {
-            a_set.add(A[i]);
-            b_set.add(B[i]);
-            Set<Integer> temp = new HashSet<>(a_set);
-            temp.retainAll(b_set);
-            count[i] = temp.size(); 
+            cache[A[i] - 1] = cache[A[i] - 1] + 1;
+            if(cache[A[i] - 1] == 2) {
+                sameNumsCount++;
+            }
+            cache[B[i] - 1] = cache[B[i] - 1] + 1;
+            if(cache[B[i] - 1] == 2) {
+                sameNumsCount++;
+            }
+            count[i] = sameNumsCount; 
         }
 
         return count;
