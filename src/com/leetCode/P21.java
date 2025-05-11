e64543b66464d07db7244871d53d75f1522ff7e9
Swapnil Railkar <swapnilrailkar01@gmail.com>
2025-05-11 22:21:35 +0530

    P21 accepted (rt: 0ms)

--- src/com/leetCode/P21.java
+++ src/com/leetCode/P21.java
@@ -4,50 +4,40 @@ import com.leetCode.DsNodes.ListNode;
 
 public class P21 {
     public ListNode solution(ListNode head1, ListNode head2) {
-        ListNode mergedList = null;
-
-        // initialize mergedList
-        if(head1 != null && head2 != null) {
-            // compare values and assign smallest
+    	ListNode head = null;
+        if(head1 == null) {
+            return head2;
+        } else if(head2 == null) {
+            return head1;
+        } else {
+            // init head
             if(head1.val < head2.val) {
-                mergedList = new ListNode(head1.val);
+                head = new ListNode(head1.val);
                 head1 = head1.next;
             } else {
-                mergedList = new ListNode(head2.val);
-                head2 = head2.next; 
-            }
-        } else if(head1 == null) {
-            // first list is empty/ null so return head2
-            return head2;
-        } else if (head2 == null) {
-            // second list is empty/null so return head1
-            return head1;
-        }
-
-
-        ListNode head = mergedList;
-        while(head1 != null || head2 != null) {
-            if(head1 != null && head2!= null) {
-                if(head1.val < head2.val) {
-                    mergedList.next = new ListNode(head1.val);
-                    head1 = head1.next;
-                    mergedList = mergedList.next;
-                } else {
-                    mergedList.next = new ListNode(head2.val);
-                    head2 = head2.next;
-                    mergedList = mergedList.next;
-                }
-            } else if(head1 == null) {
-                mergedList.next = new ListNode(head2.val);
+                head = new ListNode(head2.val);
                 head2 = head2.next;
-                mergedList = mergedList.next;
-            } else if(head2 == null) {
-                mergedList.next = new ListNode(head1.val);
-                head1 = head1.next;
-                mergedList = mergedList.next;
+            }
+            ListNode currHead = head;
+            while(head1 != null || head2 != null) {
+                if(head1 != null && head2 != null) {
+                    if(head1.val < head2.val) {
+                        currHead.next = new ListNode(head1.val);
+                        head1 = head1.next;
+                    } else {
+                        currHead.next = new ListNode(head2.val);
+                        head2 = head2.next;
+                    }
+                } else if(head1 == null) {
+                    currHead.next = new ListNode(head2.val);
+                    head2 = head2.next;
+                } else {
+                    currHead.next = new ListNode(head1.val);
+                    head1 = head1.next;
+                }
+                currHead = currHead.next;
             }
         }
-
         return head;
     }
 }
