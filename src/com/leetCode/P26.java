c5eb84b614b5f4e747a3eaf3e120bb2f8f301c92
Swapnil Railkar <swapnilrailkar01@gmail.com>
2025-05-15 18:48:58 +0530

    P26 accepted(rt: 0ms)

--- src/com/leetCode/P26.java
+++ src/com/leetCode/P26.java
@@ -2,25 +2,14 @@ package com.leetCode;
 
 public class P26 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-        int[] nums= {0,0,1,1,1,2,2,3,3,4};
-        int og = countOg(nums);
-        System.out.println(og);
-        
-	}
-
-	private static int countOg(int[] nums) {
-		// TODO Auto-generated method stub
-		int k = 0;
-		
-		for(int i=1; i< nums.length;i++) {
-			if(nums[i] != nums[i-1]) {
-				nums[k] = nums[i];
-				k++;
-			}
-		}
-		return k;
-	}
+	public int removeDuplicates(int[] nums) {
+        int lastModifiedIndex = 0;
+        for(int i = 1; i< nums.length; i++) {
+            if(nums[i] != nums[i - 1]) {
+                nums[++lastModifiedIndex] = nums[i];
+            }
+        }
+        return ++lastModifiedIndex;
+    }
 
 }
