f0115c0ee5ffd0f50824f5e44529eef2d27d93e5
Swapnil Railkar <swapnilrailkar01@gmail.com>
2025-05-19 14:56:13 +0530

    P3024 accepted(rt: 0ms)

---
+++ src/com/leetCode/P3024.java
@@ -0,0 +1,16 @@
+package com.leetCode;
+
+public class P3024 {
+	public String solution(int[] nums) {
+        if (nums[0] + nums[1] <= nums[2] || nums[0] + nums[2] <= nums[1] || nums[1] + nums[2] <= nums[0]) {
+            return "none";
+        }
+        if (nums[0] == nums[1] && nums[1] == nums[2]) {
+            return "equilateral";
+        } else if (nums[0] == nums[1] || nums[1] == nums[2] || nums[0] == nums[2]) {
+            return "isosceles";
+        } else {
+            return "scalene";
+        }
+    }
+}
