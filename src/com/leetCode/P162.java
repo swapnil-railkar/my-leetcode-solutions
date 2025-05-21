a372b2284aa00075ed811ec2ec57ec29c368d229
Swapnil Railkar <swapnilrailkar01@gmail.com>
2025-05-21 18:38:00 +0530

    P162 accepted(rt: 0ms)

--- src/com/leetCode/P162.java
+++ src/com/leetCode/P162.java
@@ -3,37 +3,25 @@ package com.leetCode;
 public class P162 {
 	
 	public int solution(int[] nums) {
-		return getPeakNumIndex(nums, 0, nums.length - 1);
-	}
-	
-	private int getPeakNumIndex(int[] nums, int startIndex, int endIndex) {
-		if(startIndex >= endIndex) {
-			if(isPeakNum(startIndex, nums)) {
-				return startIndex;
-			}
-			return -1;
-		} else {
-			int mid = (startIndex + endIndex) / 2;
-			if(isPeakNum(mid, nums)) {
-				return mid;
-			} else {
-				int leftPeak = getPeakNumIndex(nums, startIndex, mid - 1);
-				if(leftPeak == -1) {
-					return getPeakNumIndex(nums, mid + 1, endIndex);
-				} else {
-					return leftPeak;
-				}
-			}
-		}
-	}
-	
-	private boolean isPeakNum(int index, int[] nums) {
-		if(index == 0) {
-			return nums[index] > nums[index + 1];
-		} else if(index == nums.length - 1) {
-			return nums[index] > nums[index - 1];
-		} else {
-			return nums[index] > nums[index + 1] && nums[index] > nums[index - 1];
-		}
+		int index = nums.length / 2;
+        while(index >= 0 && index < nums.length) {
+            int left = (index - 1) >= 0 ? nums[index - 1] : Integer.MIN_VALUE;
+            int right = (index + 1) < nums.length ? nums[index + 1] : Integer.MIN_VALUE;
+            if(left <= nums[index] && right <= nums[index]) {
+                // peak index found
+                return index;
+            } else if(left < nums[index] && right > nums[index]) {
+                index++;
+            } else if(left > nums[index] && right < nums[index]) {
+                index--;
+            } else {
+                if(right > left) {
+                    index++;
+                } else {
+                    index--;
+                }
+            }
+        }
+        return -1;
 	}
 }
