48f7dc4680524340d1f044da382f0b50b5323652
Swapnil Railkar <swapnilrailkar01@gmail.com>
2025-05-24 21:20:59 +0530

    P380 accepted(rt: 27ms)

--- src/com/leetCode/P380.java
+++ src/com/leetCode/P380.java
@@ -1,40 +1,45 @@
 package com.leetCode;
 
 import java.util.ArrayList;
-import java.util.HashSet;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Set;
+import java.util.Map;
 
 public class P380 {
-	Set<Integer> set = null;
-    List<Integer> randomizedSet = null;
+	List<Integer> list = null;
+    Map<Integer, Integer> valueIndexMap = null;
     
     public P380() {
-        set = new HashSet<>();
-        randomizedSet = new ArrayList<>();
+        list = new ArrayList<>();
+        valueIndexMap = new HashMap<>();
     }
     
     public boolean insert(int val) {
-        if(set.contains(val)) {
+        if(valueIndexMap.containsKey(val)) {
             return false;
         }
-        set.add(val);
-        randomizedSet.add(val);
+        list.add(val);
+        valueIndexMap.put(val, list.size() - 1);
         return true;
     }
     
     public boolean remove(int val) {
-        if(!set.contains(val)) {
+        if(!valueIndexMap.containsKey(val)) {
             return false;
         }
-        set.remove(val);
-        Integer wrapper = Integer.valueOf(val);
-        randomizedSet.remove(wrapper);
+        int index = valueIndexMap.get(val);
+        int lastVal = list.get(list.size() - 1);
+        
+        list.set(index, lastVal);
+        valueIndexMap.put(lastVal, index);
+
+        valueIndexMap.remove(val);
+        list.remove(list.size() - 1);
         return true;
     }
     
     public int getRandom() {
-        int randomIndex = (int)(Math.random() * randomizedSet.size());
-        return randomizedSet.get(randomIndex);
+        int randomIndex = (int)(Math.random() * list.size());
+        return list.get(randomIndex);
     }
 }
