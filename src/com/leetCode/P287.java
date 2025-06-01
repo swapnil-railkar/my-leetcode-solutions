01f3dfca3d82abc0887aeb885913caee78080782
Swapnil Railkar <swapnilrailkar01@gmail.com>
2025-06-01 19:47:42 +0530

    P287 accepted(rt:4ms)

--- src/com/leetCode/P287.java
+++ src/com/leetCode/P287.java
@@ -2,23 +2,18 @@ package com.leetCode;
 
 public class P287 {
 
-    public int solution(int[] nums) {
-        int count = 0;
-        int currIndex = 0;
-        for(int i=0; i< nums.length; i++) {
-            if (i > 0) {
-                if (nums[i] != nums[i-1]) {
-                    count = 0;
-                } else {
-                    count++;
-                }
-            }
-
-            if (count < 2) {
-                nums[currIndex] = nums[i];
-                currIndex++;
-            }
-        }
-        return currIndex;
-    }
+	public int solution(int[] nums) {
+	       int slow = nums[0];
+	       int fast = nums[0];
+	       do {
+	        slow = nums[slow];
+	        fast = nums[nums[fast]];
+	       } while(slow != fast);
+	       slow = nums[0];
+	       while(slow != fast) {
+	        slow = nums[slow];
+	        fast = nums[fast];
+	       }
+	       return slow;
+	}
 }
