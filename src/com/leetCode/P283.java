036992738b61664191a7e383f806d835aff9058d
Swapnil Railkar <swapnilrailkar01@gmail.com>
2025-06-03 00:15:32 +0530

    P283 accepted(rt: 1ms)

---
+++ src/com/leetCode/P283.java
@@ -0,0 +1,15 @@
+package com.leetCode;
+
+public class P283 {
+	public void solution(int[] nums) {
+        int insertPos = 0;
+        for (int num : nums) {
+            if (num != 0) {
+                nums[insertPos++] = num;
+            }
+        }
+        while (insertPos < nums.length) {
+            nums[insertPos++] = 0;
+        }
+    }
+}
