30181232d73df3e6c7866a0afae31122d3dd33a2
Swapnil Railkar <swapnilrailkar01@gmail.com>
2025-06-04 19:53:22 +0530

    P207 accepted(rt: 7ms)

--- src/com/leetCode/P207.java
+++ src/com/leetCode/P207.java
@@ -4,93 +4,40 @@ import java.util.*;
 
 public class P207 {
 
-    public static void main(String[] args) {
-        int[][] prerequisites = {
-                {1,0}
-        }; // true
-        System.out.println(canFinish(prerequisites));
-    }
-
-    private static boolean canFinish(int[][] prerequisites) {
-        if (prerequisites.length == 0) {
-            return true;
+	public boolean solution(int numCourses, int[][] prerequisites) {
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+        for(int i = 0; i< numCourses; i++) {
+            graph.put(i, new ArrayList<>());
         }
-        Map<Integer, List<Integer>> map = new HashMap<>();
-        Set<Integer> allNodes = new HashSet<>();
-        for (int[] prerequisite : prerequisites) {
-            Integer key = prerequisite[0];
-            Integer value = prerequisite[1];
-            allNodes.add(key);
-            allNodes.add(value);
-
-            if (key.equals(value)) {
-                // self loop
+        for(int[] req : prerequisites) {
+            graph.get(req[0]).add(req[1]);
+        }
+        Set<Integer> visiting = new HashSet<>();
+        Set<Integer> visited = new HashSet<>();
+        for(int i = 0; i< numCourses; i++) {
+            if(!canComplete(graph, i, visiting, visited)) {
                 return false;
             }
-
-            // New pair detected, populate graph
-            List<Integer> values = map.get(key);
-            if (values == null || values.isEmpty()) {
-                values = new ArrayList<>();
-            }
-            values.add(value);
-            map.put(key, values);
-
         }
-
-        return !checkNodes(allNodes, map);
+        return true;
     }
 
-    private static boolean checkNodes(Set<Integer> allNodes, Map<Integer, List<Integer>> map) {
-        Set<Integer> whiteSet = allNodes;
-        Set<Integer> greySet = new HashSet<>();
-        Set<Integer> blackSet = new HashSet<>();
-
-        while (whiteSet.size() > 0) {
-            Integer node = whiteSet.iterator().next();
-            if (!map.containsKey(node)) {
-                swapSet(node, whiteSet, blackSet);
-                continue;
+    private boolean canComplete(Map<Integer, List<Integer>> graph, int node, Set<Integer> visiting, Set<Integer> visited) {
+        if(visiting.contains(node)) {
+            return false;
+        } else if(visited.contains(node)) {
+            return true;
+        } else {
+            List<Integer> neigh = graph.get(node);
+            visiting.add(node);
+            for(int n : neigh) {
+                if(!canComplete(graph, n, visiting, visited)) {
+                    return false;
+                }
             }
-            if (cycleDetected(node, map, whiteSet, greySet, blackSet)) {
-                return true;
-            }
+            visiting.remove(node);
+            visited.add(node);
+            return true;
         }
-        return false;
-    }
-
-    // True -> cycle
-    // false -> no cycle
-    // This approach is from : https://youtu.be/rKQaZuoUR4M?si=XtxFZAeuKA1-_SLj  (3 set approach to find cycle in directed graph)
-    private static boolean cycleDetected(Integer key, Map<Integer, List<Integer>> map, Set<Integer> whiteSet, Set<Integer> greySet, Set<Integer> blackSet) {
-
-        swapSet(key, whiteSet, greySet);
-
-        for(Integer neigh : map.get(key)) {
-            if (greySet.contains(neigh)) {
-                return true;
-            }
-
-            if (blackSet.contains(neigh)) {
-                continue;
-            }
-
-            if (!map.containsKey(neigh)) {
-                swapSet(neigh, whiteSet, blackSet);
-                return cycleDetected(key, map, whiteSet, greySet, blackSet);
-            }
-
-            if (cycleDetected(neigh, map, whiteSet, greySet, blackSet)) {
-                return true;
-            }
-        }
-
-        swapSet(key, greySet, blackSet);
-        return false;
-    }
-
-    private static void swapSet(Integer value, Set<Integer> fromSet, Set<Integer> toSet) {
-        fromSet.remove(value);
-        toSet.add(value);
     }
 }
