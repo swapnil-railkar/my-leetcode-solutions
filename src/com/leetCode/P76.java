b9abfe8dbd5841d8bcb7fddcabc37b1471b78b7a
swapnil-railkar <swapnilrailkar01@gmail.com>
2025-08-10 15:10:33 +0530

    P76 accepted(rt: 408ms)

--- src/com/leetCode/P76.java
+++ src/com/leetCode/P76.java
@@ -5,44 +5,57 @@ import java.util.Map;
 
 public class P76 {
 	public String solution(String str, String target) {
-        if(target.length() > str.length()) {
+        if (target.length() > str.length()) {
             return "";
         }
-        Map<Character, Integer> targetCharCount = new HashMap<>();
-        for(int i =0; i< target.length(); i++) {
-            int count = targetCharCount.getOrDefault(target.charAt(i), 0) + 1;
-            targetCharCount.put(target.charAt(i), count);
+        Map<Character, Integer> targetCount = new HashMap<>();
+        for (int i = 0; i < target.length(); i++) {
+            int count = targetCount.getOrDefault(target.charAt(i), 0) + 1;
+            targetCount.put(target.charAt(i), count);
         }
-        int req = targetCharCount.size(), have = 0;
+
         int left = 0, resLeft = 0;
         int minDist = Integer.MAX_VALUE;
-        Map<Character, Integer> windowCountMap = new HashMap<>();
-        for(int right = 0; right < str.length(); right++) {
-        	char c = str.charAt(right);
-        	windowCountMap.put(c, windowCountMap.getOrDefault(c, 0) + 1);
-        	
-        	if(targetCharCount.containsKey(c) && windowCountMap.get(c) == targetCharCount.get(c)) {
-        		have++;
-        	}
-        	while(have == req) {
-        		if(right - left + 1 < minDist) {
-        			minDist = right - left + 1;
-        			resLeft = left;
-        		}
-        		char leftChar = str.charAt(left);
-        		windowCountMap.put(leftChar, windowCountMap.get(leftChar) - 1);
-        		if(targetCharCount.containsKey(leftChar) && windowCountMap.get(leftChar) < targetCharCount.get(leftChar)) {
-        			have--;
-        		}
-        		left++;
-        	}
+        Map<Character, Integer> windowCount = new HashMap<>();
+        for (int right = 0; right < str.length(); right++) {
+            char c = str.charAt(right);
+            if (targetCount.containsKey(c)) {
+                windowCount.put(c, windowCount.getOrDefault(c, 0) + 1);
+                if (valid(targetCount, windowCount)) {
+                    // shrink from left until this is true.
+                    while (valid(targetCount, windowCount)) {
+                        char leftChar = str.charAt(left);
+                        if (targetCount.containsKey(leftChar)) {
+                            int value =  windowCount.get(leftChar) - 1 < 0 ? 0 : windowCount.get(leftChar) - 1;
+                            windowCount.put(leftChar, value);
+                        }
+                        left++;
+                    }
+                    left--;
+                    char leftChar = str.charAt(left);
+                    windowCount.put(leftChar, windowCount.get(leftChar) + 1);
+                    if (right - left + 1 < minDist) {
+                        minDist = right - left + 1;
+                        resLeft = left;
+                    }
+                }
+            }
         }
         return minDist == Integer.MAX_VALUE ? "" : buildString(resLeft, resLeft + minDist, str);
     }
 
+    private boolean valid(Map<Character, Integer> target, Map<Character, Integer> window) {
+        for(char key : target.keySet()) {
+            if(target.get(key) > window.getOrDefault(key, 0)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     private String buildString(int left, int right, String str) {
         StringBuilder ans = new StringBuilder();
-        for(int i = left; i< right; i++) {
+        for (int i = left; i < right; i++) {
             ans.append(str.charAt(i));
         }
         return ans.toString();
--- src/com/leetCode/Question.java
+++ src/com/leetCode/Question.java
@@ -10,13 +10,8 @@ import com.leetCode.DsNodes.TreeNode;
 public class Question {
 	public static void main(String[] args) {
 		LocalDateTime startTime = LocalDateTime.now();
-		P253 question = new P253();
-		int answer = question.solution(Arrays.asList(new int[] {1,5},
-				new int[] {5,10},
-				new int[] {10,15},
-				new int[] {15,20},
-				new int[] {1, 20},
-				new int[] {2, 6}));
+		P76 question = new P76();
+		String answer = question.solution("aaaaaaaaaaaabbbbbcdd", "abcdd");
 		LocalDateTime endTime = LocalDateTime.now();
 		System.out.println(answer);
 		//printTree(answer);
