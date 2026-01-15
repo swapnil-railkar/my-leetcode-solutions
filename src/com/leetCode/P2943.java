c751a1197bbc2b6379c66f087cf7868bc3e04f4f
swapnil-railkar <swapnilrailkar01@gmail.com>
2026-01-15 22:30:02 +0530

    P2943 accepted(rt:2ms)

--- src/com/leetCode/P2943.java
+++ src/com/leetCode/P2943.java
@@ -1,5 +1,27 @@
 package com.leetCode;
 
-public class P2943 {
+import java.util.Arrays;
 
+public class P2943 {
+	public int solution(int horizontals, int verticals, int[] hBars, int[] vBars) {
+        int vGap = getMaxGap(vBars);
+        int hGap = getMaxGap(hBars);
+        int side = Math.min(vGap, hGap);
+        return side*side;
+    }
+
+    private int getMaxGap(int[] bars) {
+        Arrays.sort(bars);
+        int max = 1;
+        int count = 1;
+        for(int i = 1; i< bars.length; i++) {
+            if(bars[i] == bars[i - 1] + 1) {
+                count++;
+                max = Math.max(count, max);
+            } else {
+                count = 1;
+            }
+        }
+        return max + 1;
+    }
 }
